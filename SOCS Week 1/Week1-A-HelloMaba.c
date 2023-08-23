665185cb71996bcecf17680aab0bf4a4659acf9a
znskuy <120673199+znskuy@users.noreply.github.com>
2023-08-23 08:48:52 +0700

    Week1-A-HelloMaba.c

--- SOCS Week 1/Week1-A-HelloMaba.c
+++ SOCS Week 1/Week1-A-HelloMaba.c
@@ -2,7 +2,6 @@
 
 	<< Hello Maba >>
 
-	Congratulations for officially become a BINUS University student.
 	This is a test problem for you to test on, and today’s “After Class Review Quiz” result won’t affect your Assignment Score.
 	So try to learn as much as you can about this system you will be using in this 1st semester.
 
@@ -13,9 +12,6 @@
 #include<stdio.h>
 
 int main(){
-
 	printf("Hello Maba!\n");
-
   	return 0;
-
 }
